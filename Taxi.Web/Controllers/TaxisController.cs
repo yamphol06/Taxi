f3b91844ce741541a359f4fa4c6b68659dd3f478
EUDES YOAM PRESIGA HOLGUIN <eudespresiga122064@correo.itm.edu.co>
2020-02-12 14:42:10 -0500

    Improve the taxis controller

---
+++ Taxi.Web/Controllers/TaxisController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Taxi.Web.Data;
+using Taxi.Web.Data.Entities;
+
+namespace Taxi.Web.Controllers
+{
+    public class TaxisController : Controller
+    {
+        private readonly DataContext _context;
+        private TaxiEntity taxiEntity;
+
+        public TaxisController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Taxis
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Taxis.ToListAsync());
+        }
+
+        // GET: Taxis/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            TaxiEntity taxiEntity = await _context.Taxis.FindAsync(id);
+            if (taxiEntity == null)
+            {
+                return NotFound();
+            }
+
+            return View(taxiEntity);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        } 
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(TaxiEntity taxiEntity)
+        {
+            if (ModelState.IsValid)
+            {
+                taxiEntity.Plaque = taxiEntity.Plaque.ToUpper();
+                _context.Add(taxiEntity);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(taxiEntity);
+        }
+
+        // GET: Taxis/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var taxiEntity = await _context.Taxis.FindAsync(id);
+            if (taxiEntity == null)
+            {
+                return NotFound();
+            }
+            return View(taxiEntity);
+        }
+
+        // POST: Taxis/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, TaxiEntity taxiEntity)
+        {
+            if (id != taxiEntity.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                taxiEntity.Plaque = taxiEntity.Plaque.ToUpper();
+                _context.Update(taxiEntity);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(taxiEntity);
+        }
+
+        // GET: Taxis/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            TaxiEntity taxiEntity = await _context.Taxis.FindAsync(id);
+            if (taxiEntity == null)
+            {
+                return NotFound();
+            }
+
+            _context.Taxis.Remove(taxiEntity);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Taxi.Web/Data/Entities/TaxiEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taxi.Web.Data.Entities
+{
+    public class TaxiEntity
+    {
+        public int Id { get; set; }
+
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "The {0} field must have {1} characters.")]
+        [RegularExpression(@"^([A-Za-z]{3}\d{3})$", ErrorMessage = "The field {0} must start with three characters and ends with three numbers.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Plaque { get; set; }
+    }
+}
