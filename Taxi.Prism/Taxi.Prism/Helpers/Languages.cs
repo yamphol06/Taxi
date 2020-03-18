2822e4d22648747add7db7af4de93cdefab96bf5
EUDES YOAM PRESIGA HOLGUIN <eudespresiga122064@correo.itm.edu.co>
2020-03-18 16:50:47 -0500

    Multi Language 1

---
+++ Taxi.Prism/Taxi.Prism/Helpers/Languages.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using Taxi.Prism.Interfaces;
+using Taxi.Prism.Resources;
+using Xamarin.Forms;
+
+namespace Taxi.Prism.Helpers
+{
+    public static class Languages
+    {
+        static Languages()
+        {
+            CultureInfo ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+            Resource.Culture = ci;
+            Culture = ci.Name;
+            DependencyService.Get<ILocalize>().SetLocale(ci);
+        }
+
+        public static string Culture { get; set; }
+
+        public static string Accept => Resource.Accept;
+
+        public static string ConnectionError => Resource.ConnectionError;
+
+        public static string Error => Resource.Error;
+
+        public static string PlaqueError1 => Resource.PlaqueError1;
+
+        public static string PlaqueError2 => Resource.PlaqueError2;
+
+        public static string TaxiHistory => Resource.TaxiHistory;
+    }
+}
