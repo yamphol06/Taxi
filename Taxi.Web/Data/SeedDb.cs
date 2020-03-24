f1ab4c80641d7b3c25e6634f6923fce3c23acc58
EUDES YOAM PRESIGA HOLGUIN <eudespresiga122064@correo.itm.edu.co>
2020-03-24 17:38:57 -0500

    API to get the user group from a user

--- Taxi.Web/Data/SeedDb.cs
+++ Taxi.Web/Data/SeedDb.cs
@@ -13,7 +13,9 @@ namespace Taxi.Web.Data
         private readonly DataContext _dataContext;
         private readonly IUserHelper _userHelper;
 
-        public SeedDb(DataContext dataContext, IUserHelper userHelper)
+        public SeedDb(
+            DataContext dataContext,
+            IUserHelper userHelper)
         {
             _dataContext = dataContext;
             _userHelper = userHelper;
@@ -24,20 +26,53 @@ namespace Taxi.Web.Data
             await _dataContext.Database.EnsureCreatedAsync();
             await CheckRolesAsync();
             await CheckUserAsync("1010", "Juan", "Zuluaga", "jzuluaga55@gmail.com", "350 634 2747", "Calle Luna Calle Sol", UserType.Admin);
-            var driver = await CheckUserAsync("2020", "Juan", "Zuluaga", "jzuluaga55@hotmail.com", "350 634 2747", "Calle Luna Calle Sol", UserType.Driver);
-            var user1 = await CheckUserAsync("3030", "Juan", "Zuluaga", "carlos.zuluaga@globant.com", "350 634 2747", "Calle Luna Calle Sol", UserType.User);
-            var user2 = await CheckUserAsync("4040", "Juan", "Zuluaga", "juanzuluaga2480@correo.itm.edu.co", "350 634 2747", "Calle Luna Calle Sol", UserType.User);
+            UserEntity driver = await CheckUserAsync("2020", "Juan", "Zuluaga", "jzuluaga55@hotmail.com", "350 634 2747", "Calle Luna Calle Sol", UserType.Driver);
+            UserEntity user1 = await CheckUserAsync("3030", "Juan", "Zuluaga", "carlos.zuluaga@globant.com", "350 634 2747", "Calle Luna Calle Sol", UserType.User);
+            UserEntity user2 = await CheckUserAsync("5050", "Camila", "Cifuentes", "camila@yopmail.com", "350 634 2747", "Calle Luna Calle Sol", UserType.User);
+            UserEntity user3 = await CheckUserAsync("6060", "Sandra", "Usuga", "sandra@yopmail.com", "350 634 2747", "Calle Luna Calle Sol", UserType.User);
+            UserEntity user4 = await CheckUserAsync("7070", "Lisa", "Marin", "luisa@yopmail.com", "350 634 2747", "Calle Luna Calle Sol", UserType.User);
             await CheckTaxisAsync(driver, user1, user2);
+            await CheckUserGroups(user1, user2, user3, user4);
+        }
+
+        private async Task CheckUserGroups(UserEntity user1, UserEntity user2, UserEntity user3, UserEntity user4)
+        {
+            if (!_dataContext.UserGroups.Any())
+            {
+                _dataContext.UserGroups.Add(new UserGroupEntity
+                {
+                    User = user1,
+                    Users = new List<UserGroupDetailEntity>
+                    {
+                        new UserGroupDetailEntity { User = user2 },
+                        new UserGroupDetailEntity { User = user3 },
+                        new UserGroupDetailEntity { User = user4 }
+                    }
+                });
+
+                _dataContext.UserGroups.Add(new UserGroupEntity
+                {
+                    User = user2,
+                    Users = new List<UserGroupDetailEntity>
+                    {
+                        new UserGroupDetailEntity { User = user1 },
+                        new UserGroupDetailEntity { User = user3 },
+                        new UserGroupDetailEntity { User = user4 }
+                    }
+                });
+
+                await _dataContext.SaveChangesAsync();
+            }
         }
 
         private async Task<UserEntity> CheckUserAsync(
-                  string document,
-                  string firstName,
-                  string lastName,
-                  string email,
-                  string phone,
-                  string address,
-                  UserType userType)
+            string document,
+            string firstName,
+            string lastName,
+            string email,
+            string phone,
+            string address,
+            UserType userType)
         {
             var user = await _userHelper.GetUserAsync(email);
             if (user == null)
@@ -59,8 +94,8 @@ namespace Taxi.Web.Data
 
                 var token = await _userHelper.GenerateEmailConfirmationTokenAsync(user);
                 await _userHelper.ConfirmEmailAsync(user, token);
-
             }
+
             return user;
         }
 
@@ -72,9 +107,9 @@ namespace Taxi.Web.Data
         }
 
         private async Task CheckTaxisAsync(
-           UserEntity driver,
-           UserEntity user1,
-           UserEntity user2)
+            UserEntity driver,
+            UserEntity user1,
+            UserEntity user2)
         {
             if (!_dataContext.Taxis.Any())
             {
@@ -91,7 +126,7 @@ namespace Taxi.Web.Data
                             Qualification = 4.5f,
                             Source = "ITM Fraternidad",
                             Target = "ITM Robledo",
-                            Remarks = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in vulputate felis. Curabitur dictum tellus dolor, eget bibendum risus tincidunt id. Morbi iaculis, orci ut tempor faucibus, ipsum lorem ornare dui, sed bibendum ex nisl pulvinar diam. Quisque et ex ut lectus venenatis ultrices id non urna. In vitae ipsum ut lacus aliquam luctus ac non nulla. Integer et lectus a ex hendrerit pharetra ut id urna. Quisque vel eleifend tortor. Nunc egestas elementum lorem. In sed dui id dolor aliquet congue. Morbi at purus facilisis, vestibulum massa sed, cursus tellus.",
+                            Remarks = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer convallis finibus nisl in fringilla. Fusce et odio dolor. Aliquam venenatis libero tortor, at tincidunt neque auctor vitae. Morbi lobortis eget quam eget convallis. Morbi id magna neque. Pellentesque sit amet imperdiet lacus. Aliquam ac facilisis elit, in aliquet felis. Morbi lacinia nunc ut tristique ornare. Integer felis risus, tempor eu placerat vel, lacinia nec ipsum. Cras eleifend orci ac dictum lacinia. Cras id porta ex, quis interdum ex. Vivamus facilisis feugiat diam, vitae euismod quam dignissim sit amet. Integer egestas a velit sit amet auctor. Pellentesque mollis nisl in nibh viverra posuere. Nulla facilisi.",
                             User = user1
                         },
                         new TripEntity
@@ -101,7 +136,7 @@ namespace Taxi.Web.Data
                             Qualification = 4.8f,
                             Source = "ITM Robledo",
                             Target = "ITM Fraternidad",
-                            Remarks = "Curabitur id ante urna. Donec justo eros, eleifend ut fermentum non, scelerisque ac est. Nunc eu maximus ex, id vulputate purus. Curabitur bibendum ipsum at diam placerat, ac viverra lacus euismod. Praesent in tortor magna. Aliquam in pulvinar nisl, at ornare quam. In convallis interdum tempus. Vestibulum hendrerit ullamcorper hendrerit. Nulla sed ipsum nec purus aliquet maximus ut sed dolor. Pellentesque vestibulum, tellus id mollis vestibulum, justo massa cursus augue, venenatis consectetur elit orci nec magna. Sed eget magna lacus. Maecenas faucibus pulvinar vestibulum. Proin eu elit hendrerit, lobortis ex vel, consequat nisl. Sed porta eget libero nec rhoncus.",
+                            Remarks = "Donec non convallis elit. Donec ultricies, leo sed convallis ullamcorper, erat tortor mattis felis, ornare commodo dui lorem a erat. Proin suscipit varius purus sed viverra. Vestibulum vitae massa nec nunc hendrerit faucibus. Duis sit amet neque fringilla, interdum sem ac, imperdiet nisl. Maecenas ultrices justo felis, ac interdum sem facilisis ut. Aliquam et aliquam mi.",
                             User = user1
                         }
                     }
